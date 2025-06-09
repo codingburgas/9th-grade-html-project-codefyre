063c022d716f367afd3fcefb9b84c977e1a2b5ae
MIVasilev23 <MIVasilev23@codingburgas.bg>
2025-06-09 17:18:33 +0300

    Add map

---
+++ javascript/map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([42.5048, 27.4626], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 13,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
