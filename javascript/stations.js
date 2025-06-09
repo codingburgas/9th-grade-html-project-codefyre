170210aae5704be759b46ee8503c7a2a7be804a4
DDMeshkov23 <DDMeshkov23@codingburgas.bg>
2025-06-09 19:36:32 +0300

    Add form that inserts data into table on submit

--- javascript/stations.js
+++ javascript/stations.js
@@ -23,3 +23,36 @@ function myFunction() {
     }
   }
 }
+function tableadd(event) {
+  event.preventDefault()
+  let table = document.getElementById('myTable');
+  let form = document.forms['incidentForm'];
+  let newRow = document.createElement('tr');
+  let incidenttime = document.createElement('td');
+  incidenttime.innerText = form.incidenttime.value;
+  newRow.appendChild(incidenttime);
+  
+  let typeOfIncident = document.createElement('td');
+  typeOfIncident.innerText = form['type of incidents'].value;
+  newRow.appendChild(typeOfIncident);
+
+  let address = document.createElement('td');
+  address.innerText = form.address.value;
+  newRow.appendChild(address);
+
+  let rescueTeam = document.createElement('td');
+  rescueTeam.innerText = form['Rescue Team'].value;
+  newRow.appendChild(rescueTeam);
+
+  let typeOfVehicle = document.createElement('td');
+  typeOfVehicle.innerText = form['Type of Vehicle'].value;
+  newRow.appendChild(typeOfVehicle);
+
+  let status = document.createElement('td');
+  status.innerText = form['Status of the incident'].value;
+  newRow.appendChild(status);
+
+  table.appendChild(newRow);
+}
+
+  document.getElementById('submitButton').addEventListener('click', tableadd);
